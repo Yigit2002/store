491b2449ab2e4af76f0f6f793b03b9f34675e5fb
Yigit2002 <72856207+Yigit2002@users.noreply.github.com.>
2025-04-02 09:48:50 +0300

    Add: Dummy changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
-
+  #hello world
   config.filter_parameters = []
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
