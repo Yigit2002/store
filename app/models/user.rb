afa53a03e9fb41bcc175e474912e632818e56c7e
Yigit2002 <72856207+Yigit2002@users.noreply.github.com.>
2025-04-21 15:10:57 +0300

    Add: address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,16 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   has_secure_password
   has_many :sessions, dependent: :destroy
   has_many :cards, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :products, through: :comments
+
+
+  validates :username, uniqueness: true, allow_blank: true
+  validates :first_name, :last_name, length: { maximum: 50 }, allow_blank: true
+  validates :address, :city, :country, length: { maximum: 255 }, allow_blank: true
+  validates :telefon_numarasi, length: {maximum: 15}, allow_blank: false
 
 
   enum :role, { customer: 0, admin: 1 }, default: :customer
@@ -15,6 +18,7 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_products, through: :favorites, source: :product
   has_one :cart, dependent: :destroy
+  has_many :addresses, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
