b57149bfa03d015e2efd433fe75bb5a2d6bfcf40
Yigit2002 <72856207+Yigit2002@users.noreply.github.com.>
2025-04-24 13:53:50 +0300

    Update: View Changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,21 @@
 class User < ApplicationRecord
-
   has_secure_password
+
   has_many :sessions, dependent: :destroy
   has_many :cards, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :products, through: :comments
-
+  has_many :addresses, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_products, through: :favorites, source: :product
 
   validates :username, uniqueness: true, allow_blank: true
   validates :first_name, :last_name, length: { maximum: 50 }, allow_blank: true
-  validates :address, :city, :country, length: { maximum: 255 }, allow_blank: true
   validates :telefon_numarasi, length: {maximum: 15}, allow_blank: false
-
 
   enum :role, { customer: 0, admin: 1 }, default: :customer
 
-  has_many :favorites, dependent: :destroy
-  has_many :favorite_products, through: :favorites, source: :product
   has_one :cart, dependent: :destroy
-  has_many :addresses, dependent: :destroy
-
+  
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
